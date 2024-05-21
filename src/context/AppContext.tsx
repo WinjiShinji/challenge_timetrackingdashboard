f14d35de5246c7926174ed8305ce2924247d042c
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-05-21 08:45:55 +0100

    TypeScript - Context Fix

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,12 +1,64 @@
-import { ReactNode, createContext, useReducer } from "react"
-import { initState, timeReducer } from "../reducers/timeReducer"
+import { Dispatch, ReactNode, createContext, useReducer } from "react"
 
 type Props = {
   children: ReactNode
 }
 
-const AppContext = createContext({})
+type State = {
+  daily: boolean
+  weekly: boolean
+  monthly: boolean
+}
 
+type ReducerAction = {
+  type: string
+  payload?: string
+}
+
+type Context = {
+  state: State
+  dispatch: Dispatch<ReducerAction>
+}
+
+// ==== Reducer Logic ==== //
+const initState = {
+  daily: true,
+  weekly: false,
+  monthly: false,
+}
+
+const timeReducer = (state: State, action: ReducerAction) => {
+  switch (action.type) {
+    case "daily":
+      return {
+        ...state,
+        daily: true,
+        weekly: false,
+        monthly: false,
+      }
+    case "weekly":
+      return {
+        ...state,
+        daily: false,
+        weekly: true,
+        monthly: false,
+      }
+    case "monthly":
+      return {
+        ...state,
+        daily: false,
+        weekly: false,
+        monthly: true,
+      }
+
+    default:
+      return state
+  }
+}
+
+// ==== Context ==== //
+// @FIX: {} as Context - Double Casted!
+const AppContext = createContext<Context>({} as Context)
 export const AppProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(timeReducer, initState)
 
